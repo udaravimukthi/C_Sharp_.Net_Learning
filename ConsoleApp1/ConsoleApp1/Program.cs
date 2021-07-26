297ee0aaeb07f7faa480090cc5af17546f846b61
Udara Vimukthi <uvlakshan@gmail.com>
2021-07-27 03:46:38 +0530

    Created my first C sharp .net project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print(); 
+            int returnResult= Add(5, 6);
+            Console.WriteLine(returnResult);
+            Console.ReadLine();
+            
+        }
+
+        static void Print()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        static int Add(int a, int b)
+        {
+            int result = a + b;
+            return result;
+        }
+    }
+}
