1dcc3f73d88b70c41b0a7b9c02cdd75698d43a97
Udara Vimukthi <uvlakshan@gmail.com>
2021-08-01 19:14:14 +0530

    Added some required files of thw project

---
+++ ConsoleApp2/ConsoleApp2/Worker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Worker
+    {
+        public void Print()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        public int Add(int a, int b)
+        {
+            int result = a + b;
+            return result;
+        }
+    }
+}
